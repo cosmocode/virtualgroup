d60ee7224b2bfcea95507c9c795efe134adbeda2
Dominik Eckelmann <deckelmann@gmail.com>
2013-08-05 18:16:18 +0200

    Added russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Russian language file for Virtualgroup Plugin
+ *
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+
+$lang['menu']         = 'Виртуальные группы';
+
+$lang['nouser']       = 'Вы должны указать имя пользователя.';
+$lang['nogrp']        = 'Вы должны указать название группы.';
+
+$lang['editgroup']    = 'Редактировать группу';
+$lang['addgroup']     = 'Добавить группу';
+$lang['edituser']     = 'Редактировать пользователя';
+$lang['adduser']      = 'Добавить пользователя в группу';
+
+$lang['user']         = 'Пользователь';
+$lang['grp']          = 'Группа';
+
+$lang['add']          = 'Добавить';
+
+$lang['users']        = 'Пользователи';
+$lang['grps']         = 'Группы';
+
+$lang['del']          = 'Удалить';
+$lang['edit']         = 'Редактировать';
+
+$lang['change']       = 'Изменить';
+
+$lang['usercharerr']  = 'Изменения не сохранены. Возможно, что имя пользователя содержит символы, не поддерживаемые бэкендом аутентификации.';
+$lang['grpcharerr']   = 'Изменения не сохранены. Возможно, что название группы содержит символы, не поддерживаемые бэкендом аутентификации.';
