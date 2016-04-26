69975f1a1fa22a0df5329527e0ecef2fb24e01d9
Milan Hanik <milan.hanik@gmail.com>
2016-04-26 15:30:04 +0200

    translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -1,27 +1,24 @@
 <?php
 
-$lang['menu'] = 'Virtuální skupiny';
-
-
-$lang['nouser'] = 'Musíte zadat uživatelské jméno.';
-$lang['nogrp']  = 'Musíte zadat název skupiny.';
-
-$lang['editgroup'] = 'Editovat Skupina';
-$lang['edituser'] = 'Editovat uživatele';
-$lang['adduser']  = 'Přidat uživatele do skupiny';
-
-$lang['user'] = 'Uživatel';
-$lang['grp']  = 'Skupina';
-
-$lang['add'] = 'Přidat';
-
-$lang['users'] = 'Uživatelé';
-$lang['grps'] = 'Skupiny';
-
-$lang['del'] = 'Smazat';
-$lang['edit'] = 'Editovat';
-
-$lang['change'] = 'Změnit';
-
-$lang['usercharerr'] = 'Nebylo možné uložit změny. Možná uživatelské jméno obsahuje znaky, které autentizační zdroj nepovoluje?';
-$lang['grpcharerr'] = 'Nebylo možné uložit změny. Možná název skupiny obsahuje znaky, které autentizační zdroj nepovoluje?';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Milan Hanik <milan.hanik@gmail.com>
+ */
+$lang['menu']                  = 'Virtuální skupiny';
+$lang['nouser']                = 'Musíte zadat uživatelské jméno.';
+$lang['nogrp']                 = 'Musíte zadat název skupiny.';
+$lang['editgroup']             = 'Editovat Skupina';
+$lang['addgroup']              = 'Přidat skupinu';
+$lang['edituser']              = 'Editovat uživatele';
+$lang['adduser']               = 'Přidat uživatele do skupiny';
+$lang['user']                  = 'Uživatel';
+$lang['grp']                   = 'Skupina';
+$lang['add']                   = 'Přidat';
+$lang['users']                 = 'Uživatelé';
+$lang['grps']                  = 'Skupiny';
+$lang['del']                   = 'Smazat';
+$lang['edit']                  = 'Editovat';
+$lang['change']                = 'Změnit';
+$lang['usercharerr']           = 'Nebylo možné uložit změny. Možná uživatelské jméno obsahuje znaky, které autentizační zdroj nepovoluje?';
+$lang['grpcharerr']            = 'Nebylo možné uložit změny. Možná název skupiny obsahuje znaky, které autentizační zdroj nepovoluje?';
