6dedbf12b13815a894218e0a92010cfddc02175a
Tor Härnqvist <tor@harnqvist.se>
2017-12-23 18:45:35 +0100

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['menu']                  = 'Virtuella grupper';
+$lang['nouser']                = 'Du måste ange ett användarnamn.';
+$lang['nogrp']                 = 'Du måste ange ett gruppnamn.';
+$lang['editgroup']             = 'Redigera grupp';
+$lang['addgroup']              = 'Lägg till en grupp';
+$lang['edituser']              = 'Redigera användare';
+$lang['adduser']               = 'Lägg till användare till en grupp';
+$lang['user']                  = 'Användare';
+$lang['grp']                   = 'Grupp';
+$lang['add']                   = 'Lägg till';
+$lang['users']                 = 'Användare';
+$lang['grps']                  = 'Grupper';
+$lang['del']                   = 'Ta bort';
+$lang['edit']                  = 'Redigera';
+$lang['change']                = 'Ändra';
+$lang['usercharerr']           = 'Ändringarna kunde inte sparas. Kanske innehåller användarnamnet ett tecken som inte godkänns i autentiseringsprocessen.';
+$lang['grpcharerr']            = 'Ändringarna kunde inte sparas. Kanske innehåller gruppnamnet ett tecken som inte godkänns i autentiseringsprocessen.';
