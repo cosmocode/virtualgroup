bba478f71456171fdb7d0fb1354855e3f838a36f
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 16:10:21 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['menu']                  = 'Grupos virtuais';
+$lang['nouser']                = 'Você tem que fornecer um nome de usuário';
+$lang['nogrp']                 = 'Você tem que fornecer um nome de grupo';
+$lang['editgroup']             = 'Editar grupo';
+$lang['addgroup']              = 'Adicionar grupo';
+$lang['edituser']              = 'Editar usuário';
+$lang['adduser']               = 'Adicionar usuário em um grupo';
+$lang['user']                  = 'Usuário';
+$lang['grp']                   = 'Grupo';
+$lang['add']                   = 'Adicionar';
+$lang['users']                 = 'Usuários';
+$lang['grps']                  = 'Grupos';
+$lang['del']                   = 'Delete';
+$lang['edit']                  = 'Editar';
+$lang['change']                = 'Mudar';
+$lang['usercharerr']           = 'Não é possível salvar as alterações. Talvez o seu usuário esteja usando um caractere que não seja permitido pelo backend de autenticação.';
+$lang['grpcharerr']            = 'Não é possível salvar as alterações. Talvez seu grupo esteja usando um caractere que não seja permitido pelo backend de autenticação.';
