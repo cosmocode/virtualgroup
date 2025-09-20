a0e6e6b048292b53939b6bebba047cf8db2aae45
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2025-09-20 17:55:05 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,15 +2,24 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  * @author NicolasFriedli <nicolas@theologique.ch>
  */
 $lang['menu']                  = 'Groupes virtuels';
+$lang['byuser']                = 'Par utilisateur';
+$lang['bygroup']               = 'Par groupe';
+$lang['addUserGroups']         = 'Ajouter les groupes à l\'utilisateur';
+$lang['editUserGroups']        = 'Éditer les groupes de l\'utilisateur';
+$lang['addGroupUsers']         = 'Ajouter les utilisateur au groupe';
+$lang['editGroupUsers']        = 'Éditer les utilisateurs du groupe';
 $lang['user']                  = 'Utilisateur';
-$lang['grp']                   = 'Groupe';
-$lang['add']                   = 'Ajouter';
 $lang['users']                 = 'Utilisateurs';
+$lang['usersSample']           = 'utilisateur1, utilisateur2, utilisateur3';
+$lang['grp']                   = 'Groupe';
 $lang['grps']                  = 'Groupes';
+$lang['grpsSample']            = 'groupe1, groupe2, groupe3';
+$lang['add']                   = 'Ajouter';
 $lang['del']                   = 'Supprimer';
 $lang['edit']                  = 'Éditer';
 $lang['change']                = 'Modifier';
